4a1a4c32694a50c6662e83333f34b34d8f3a55f0
AlexanderVak <mamorunokitsune@gmail.com>
2021-07-28 15:10:49 +0300

    add finnished winner function

--- game.js
+++ game.js
@@ -1,5 +1,27 @@
-export function winner(p1, p2) {
+// export const rock = 0
+// export const paper = 1
+// export const scissors = 2
+
+export let players = new Map()
+// const signs = ['rock', 'paper', 'scissors']
+
+export const signs = new Map()
+signs.set(0, 'rock')
+signs.set(1, 'paper')
+signs.set(2, 'scissors')
+
+players.set('player1', 0)
+players.set('player2', 2)
+
+export function winner(players) {
+    let iterator = players.values()
+    let p1 = iterator.next().value
+    let p2 = iterator.next().value
+    
     if ((p1 + 1) % 3 === p2) {
-        
-    }
+        return `${signs.get(p2)} wins`
+    } else if (p1 === p2) {
+        return 'its draw'
+    } else
+        return `${signs.get(p1)} wins`
 }
